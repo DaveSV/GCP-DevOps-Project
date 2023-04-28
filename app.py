a67820bcb2f55994b519ae9574ebb2508466345d
DaveSV <alb.saenz@gmail.com>
2023-04-27 18:47:22 -0600

    agregando imagen a index.html

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<div><img src="https://user-images.githubusercontent.com/29576337/234991334-d5598f99-09e1-4c49-a31a-c9e3b13c3190.png" alt="DevOps deployment" width="776" height="248"></div><div style=\"text-align: center;\"><h1>Hello from Dave!</h1><p>Welcome to GCP DevOps App.</p><p></p></div>'
+    return '<div style="text-align:center;"><img src="https://user-images.githubusercontent.com/29576337/234991334-d5598f99-09e1-4c49-a31a-c9e3b13c3190.png" alt="DevOps deployment" width="776" height="248"></div><div style=\"text-align: center;\"><h1>Hello from Dave!</h1><p>Welcome to GCP DevOps App.</p><p></p></div>'
 
 
 if __name__ == "__main__":
